4d93dfe502dd4ae76e47a3e6659c67ed064992d1
gabrielataideferreira <gabriel.a.ferreira@datapoint.pt>
2026-05-26 17:40:29 +0000

    cleanse

--- backend/routes/videoAnalysis.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// POST /api/video-analysis
-router.post('/', (req, res) => {
-  res.json({ message: 'Analyze video and generate recipe' });
-});
-
-module.exports = router;
